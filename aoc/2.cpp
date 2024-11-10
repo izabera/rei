545bd3501cdd9ad2817dad94fcdfc8e7b01c9df5
Isabella Bosia <izaberina@gmail.com>
2024-11-10 16:38:06 +0000

    solve d2p2

--- aoc/2.cpp
+++ aoc/2.cpp
@@ -1,11 +1,11 @@
 #include "dict.hpp"
 #include "functional.hpp"
 #include "io.hpp"
-int main() {
-    dict code = io::readline().split(",").map(fn::tonum);
 
-    code[1] = 12;
-    code[2] = 2;
+var compute(dict code, const var &val1, const var &val2) {
+    code[1] = val1;
+    code[2] = val2;
+
     for (var pos = 0; code[pos] != 99; pos += 4) {
         var lhs = code[code[pos + 1]];
         var rhs = code[code[pos + 2]];
@@ -15,5 +15,24 @@ int main() {
         else
             code[code[pos + 3]] = lhs * rhs;
     }
-    io::println(code[0]);
+    return code[0];
+}
+
+int main() {
+    dict code = io::readline().split(",").map(fn::tonum);
+
+    var part1 = compute(code, 12, 2);
+    var part2;
+
+    for (var i = 0; i <= 99; i++) {
+        for (var j = 0; j <= 99; j++) {
+            var ret = compute(code, i, j);
+            if (ret == 19690720) {
+                part2 = i * 100 + j;
+                break;
+            }
+        }
+    }
+
+    io::println({part1, part2});
 }
